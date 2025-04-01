3b0402662843716145603d823940e4bc190285b2
thecsguy09 <thecsguy09@gmail.com>
2025-04-01 22:43:25 +0530

    Create 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountID = 144565;
+let accountEmial = "kunalbang2@gmail.com"
+var acountPassword = "1234567890";
+accountCity = "Delhi";
+let accountType;
+
+console.log(accountID);
+accountCity = "Mumbai";
+
+console.table([accountID, accountEmial, acountPassword, accountCity, accountType]);
