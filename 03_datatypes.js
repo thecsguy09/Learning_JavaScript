f50858b08d9937738a34ba4820c2b4145e36b4c4
thecsguy09 <thecsguy09@gmail.com>
2025-04-01 22:43:32 +0530

    Create 03_datatypes.js

---
+++ 03_datatypes.js
@@ -0,0 +1,18 @@
+// number => 2^52
+// bigint => last mai "n" use kr lena
+// string => " "
+// boolen => true/false
+// null => its a standalone datatype, null = object
+// undefined => undefined = undefined
+// symbol => unique and immutable
+// object => object only
+// array => object only
+// function => function only
+
+
+// we convert like this
+
+let score = "33"
+let score1 = Number(score) 
+
+console.log(score1, typeof score1) // 33 number
